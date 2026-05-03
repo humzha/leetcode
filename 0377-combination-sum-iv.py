5cb3314a693a032df6a6678fdb0ec878e06dd449
mix <lilacjolt@gmail.com>
2026-05-03 12:29:23 -0400

    377

---
+++ 0377-combination-sum-iv.py
@@ -0,0 +1,21 @@
+class Solution:
+    """Solution for counting combinations that sum to a target.
+
+    Given an array of distinct integers nums and a target integer target,
+    return the number of possible combinations that add up to target.
+    Different sequences are counted as different combinations.
+    """
+
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+        # Your implementation here
+        dp = [1 if i == target else None for i in range(target + 1)]
+        def f(x: int) -> int:
+            if x < 0:
+                return 0
+            if dp[x] is None:
+                res = 0
+                for n in nums:
+                    res += f(x + n)
+                dp[x] = res
+            return dp[x]
+        return f(0)
