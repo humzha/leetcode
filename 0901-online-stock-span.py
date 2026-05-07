c6232fe9aca429c13c64f75883d82ad879750975
mix <lilacjolt@gmail.com>
2026-05-07 11:45:08 -0400

    fix 901

--- 0901-online-stock-span.py
+++ 0901-online-stock-span.py
@@ -15,10 +15,11 @@ class StockSpanner:
         # 100, 80 price = 60
         if not self.stack or self.stack[-1][0] > price:
             self.stack.append((price, 1))
+            return 1 
         # 100, 80, price = 80, 81
         span = 1
         while self.stack and self.stack[-1][0] <= price:
             _, popped_span = self.stack.pop()
             span += popped_span
-        self.stack.append(price, span)
+        self.stack.append((price, span))
         return self.stack[-1][1]
